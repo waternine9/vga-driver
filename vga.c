585d66b5e80a8369ddc128627aad57bd227a8996
waternine9 <62113486+waternine9@users.noreply.github.com>
2023-07-06 01:29:56 -0400

    Update vga.c

--- vga.c
+++ vga.c
@@ -59,6 +59,7 @@
 
 #define VGA_GRAPHICS_SET_RESET 0x00
 #define VGA_GRAPHICS_DATA_ROTATE 0x03
+#define VGA_GRAPHICS_READ_MAP_SELECT 0x04
 #define VGA_GRAPHICS_MODE 0x05
 #define VGA_GRAPHICS_MISC 0x06
 #define VGA_GRAPHICS_COLOR_DONT_CARE 0x07
@@ -141,16 +142,14 @@ void VGA_Init()
 {
     uint8_t Misc = IO_In8(VGA_MISCELLANEOUS_REG_READ);
 
-    IoAddressBit = Misc & 1;
+    IoAddressBit = 1;
 
-    Misc = 0b11000011; // Set the VSP and HSP to NEGATIVE (1) and set ERAM
+    Misc = 0b11000111; // Set the VSP and HSP to NEGATIVE (1) and set ERAM
 
     IO_Out8(VGA_MISCELLANEOUS_REG_WRITE, Misc);
 
-    IO_In8(GetInputStatus1Addr()); // Set the attributes to be indexed at the start
-    IO_Out8(VGA_ATTRIBUTE_ADDR, 0x20);
 
-    WriteCRT(VGA_CRT_VERTICAL_RETRACE_END, 0x8C);
+    WriteCRT(VGA_CRT_VERTICAL_RETRACE_END, 0x0C);
     WriteCRT(VGA_CRT_VERTICAL_RETRACE_START, 0xEA);
     WriteCRT(VGA_CRT_HORIZONTAL_TOTAL, 0x5F);
     WriteCRT(VGA_CRT_VERTICAL_TOTAL, 0x0B);
@@ -168,17 +167,21 @@ void VGA_Init()
     WriteCRT(VGA_CRT_OFFSET, 0x28);
     WriteCRT(VGA_CRT_OVERFLOW, 0x3E);
     WriteCRT(VGA_CRT_MODE, 0b11000011);
+    WriteCRT(VGA_CRT_VERTICAL_RETRACE_END, 0x8C);
 
     WriteGraphics(VGA_GRAPHICS_MISC, 0x5);
     WriteGraphics(VGA_GRAPHICS_MODE, 0b00000000);
     WriteGraphics(VGA_GRAPHICS_BIT_MASK, 0b11111111);
     WriteGraphics(VGA_GRAPHICS_DATA_ROTATE, 0);
 
-    WriteSequencer(VGA_SEQUENCER_MEMORY_MODE, 0b00000010); // Set the EM
+    WriteSequencer(VGA_SEQUENCER_MEMORY_MODE, 0b00000010);
     WriteSequencer(VGA_SEQUENCER_MAP_MASK, 0b00001111);
     WriteSequencer(VGA_SEQUENCER_CHARACTER_MAP_SELECT, 0b0);
     WriteSequencer(VGA_SEQUENCER_CLOCKING_MODE, 0b00000001);
 
+    IO_In8(GetInputStatus1Addr()); // Set the attributes to be indexed at the start
+    IO_Out8(VGA_ATTRIBUTE_ADDR, 0x20);
+    
     WriteAttribute(VGA_ATTRIBUTE_MODE_CONTROL, 0b00000001);
     WriteAttribute(VGA_ATTRIBUTE_COLOR_PLANE_ENABLE, 0b00001111);
     WriteAttribute(VGA_ATTRIBUTE_COLOR_SELECT, 0b00000000);
@@ -206,60 +209,73 @@ void VGA_SetColor(uint8_t Red, uint8_t Green, uint8_t Blue, uint8_t Alpha)
     CacheMask |= Blue ? 0b1 : 0;
 }
 
-void VGA_WritePixel(uint32_t X, uint32_t Y, uint8_t Value)
+void VGA_WritePixel(uint32_t X, uint32_t Y)
 {
     if (X >= 640) return;
     if (Y >= 480) return;
 
-    WriteSequencer(VGA_SEQUENCER_MAP_MASK, 0b00001111);
-    ((uint8_t*)0xA0000)[X / 8 + Y * 80] = 0;
-    WriteSequencer(VGA_SEQUENCER_MAP_MASK, CacheMask);
-    ((uint8_t*)0xA0000)[X / 8 + Y * 80] = Value ? 0xFF : 0x00;
+    WriteGraphics(VGA_GRAPHICS_BIT_MASK, 0b11111111);
+    
+    WriteGraphics(VGA_GRAPHICS_READ_MAP_SELECT, 0b00000000);
+    uint8_t CurrentMap0 = ((uint8_t*)0xA0000)[X / 8 + Y * 80];
+    WriteGraphics(VGA_GRAPHICS_READ_MAP_SELECT, 0b00000001);
+    uint8_t CurrentMap1 = ((uint8_t*)0xA0000)[X / 8 + Y * 80];
+    WriteGraphics(VGA_GRAPHICS_READ_MAP_SELECT, 0b00000010);
+    uint8_t CurrentMap2 = ((uint8_t*)0xA0000)[X / 8 + Y * 80];
+    WriteGraphics(VGA_GRAPHICS_READ_MAP_SELECT, 0b00000011);
+    uint8_t CurrentMap3 = ((uint8_t*)0xA0000)[X / 8 + Y * 80];
+
+    CurrentMap0 &= ~(1 << (7 - (X % 8)));
+    CurrentMap1 &= ~(1 << (7 - (X % 8)));
+    CurrentMap2 &= ~(1 << (7 - (X % 8)));
+    CurrentMap3 &= ~(1 << (7 - (X % 8)));
+    
+    if (CacheMask & 0b1) CurrentMap0 |= 1 << (7 - (X % 8));
+    if (CacheMask & 0b10) CurrentMap1 |= 1 << (7 - (X % 8));
+    if (CacheMask & 0b100) CurrentMap2 |= 1 << (7 - (X % 8));
+    if (CacheMask & 0b1000) CurrentMap3 |= 1 << (7 - (X % 8));
+
+    WriteSequencer(VGA_SEQUENCER_MAP_MASK, 0b00000001);
+    ((uint8_t*)0xA0000)[X / 8 + Y * 80] = CurrentMap0;
+    WriteSequencer(VGA_SEQUENCER_MAP_MASK, 0b00000010);
+    ((uint8_t*)0xA0000)[X / 8 + Y * 80] = CurrentMap1;
+    WriteSequencer(VGA_SEQUENCER_MAP_MASK, 0b00000100);
+    ((uint8_t*)0xA0000)[X / 8 + Y * 80] = CurrentMap2;
+    WriteSequencer(VGA_SEQUENCER_MAP_MASK, 0b00001000);
+    ((uint8_t*)0xA0000)[X / 8 + Y * 80] = CurrentMap3;
 }
 
 void VGA_Clear()
 {
     WriteSequencer(VGA_SEQUENCER_MAP_MASK, 0b00001111);
+    WriteGraphics(VGA_GRAPHICS_BIT_MASK, 0b11111111);
 
-    for (int i = 0;i < (640 / 8) * 480;i++)
+    for (int i = 0;i < 640 * 480;i++)
     {
         ((uint8_t*)0xA0000)[i] = 0x0;
     }
 
     WriteSequencer(VGA_SEQUENCER_MAP_MASK, CacheMask);
 
-    for (int i = 0;i < (640 / 8) * 480;i++)
+    for (int i = 0;i < 640 * 480;i++)
     {
         ((uint8_t*)0xA0000)[i] = 0xFF;
     }
 }
 
-void _WritePixelFast(uint32_t X, uint32_t Y, uint8_t Value)
-{
-    if (X >= 640) return;
-    if (Y >= 480) return;
-
-    ((uint8_t*)0xA0000)[X / 8 + Y * 80] = Value ? 0xFF : 0x00;
-}
-
 void VGA_WriteRect(uint32_t x, uint32_t y, uint32_t w, uint32_t h)
 {
-    WriteSequencer(VGA_SEQUENCER_MAP_MASK, 0b00001111);
-    for (int _x = x;_x < x + w;_x++)
+    WriteGraphics(VGA_GRAPHICS_BIT_MASK, 0b11111111);
+    for (int i = 0;i < 4;i++)
     {
-        for (int _y = y;_y < y + h;_y++)
+        WriteGraphics(VGA_GRAPHICS_READ_MAP_SELECT, i);
+        WriteSequencer(VGA_SEQUENCER_MAP_MASK, (1 << i));
+        for (int _x = x;_x < x + w;_x++)
         {
-            _WritePixelFast(_x, _y, 0);
+            for (int _y = y;_y < y + h;_y++)
+            {
+                ((uint8_t*)0xA0000)[_x / 8 + _y * 80] |= 1 << (7 - (_x % 8));
+            }
         }
-    }
-
-    WriteSequencer(VGA_SEQUENCER_MAP_MASK, CacheMask);
-
-    for (int _x = x;_x < x + w;_x++)
-    {
-        for (int _y = y;_y < y + h;_y++)
-        {
-            _WritePixelFast(_x, _y, 1);
-        }   
     }
 }
